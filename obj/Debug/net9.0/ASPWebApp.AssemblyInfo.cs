9cf61787de7140f6024e411b499a3b3fd8561dbd
Kevin Nguyen <kv375322@dal.ca>
2025-02-19 15:18:31 -0400

    Deleted existing database

--- obj/Debug/net9.0/ASPWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASPWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42492c1d1ac8a3263010d0009e8efa0c9233cfb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
